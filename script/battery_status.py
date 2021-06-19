eb2940cf3f9ded170a209d86716eb46554421ae0
Chaithra K C <chaithra.kc22@gmail.com>
2021-06-19 18:06:18 +0530

    notification feature enhanced

--- script/battery_status.py
+++ script/battery_status.py
@@ -9,6 +9,7 @@ from twilio.rest import Client
 class BatteryNotifier:
     def __init__(self):
         relative_filepath = os.path.join('..', 'resources', 'settings.json')
+        self.iconpath = os.path.join('..', 'resources', 'battery-charging.ico')
         with open(relative_filepath) as file_handler:
             self.settings = json.load(file_handler)
         self.client = Client(self.settings['account_sid'], self.settings['authtoken'])
@@ -20,13 +21,13 @@ class BatteryNotifier:
             if battery.percent >= self.settings['maxbattery_percentage'] and battery.power_plugged:
                 self.send_notification(battery)
             elif not battery.power_plugged:
+                self.toaster.show_toast('Battery (unplugged) ', str(battery.percent)+'%', self.iconpath)
                 exit(0)
             time.sleep(15)
 
     def send_notification(self, battery):
         msg = 'Battery ' + str(battery.percent) + '% (plugged in)'
-        iconpath = os.path.join('..', 'resources', 'battery-charging.ico')
-        self.toaster.show_toast(msg, 'disconnect!', iconpath)
+        self.toaster.show_toast(msg, 'disconnect!', self.iconpath)
         self.client.messages.create(from_=self.settings['twilio_phone'], to=self.settings['self_phone'], body=msg)
 
 
